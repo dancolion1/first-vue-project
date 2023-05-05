44c0df6f3c8c92bc224afd2bda22367b14b3563f
oleabhiele daniel donald <danieloleabhiele@gmail.com>
2023-05-05 11:55:51 +0100

    vue bugs fixedt

---
+++ src/modules/portfolio/getters.js
@@ -0,0 +1,9 @@
+export default {
+    items(state, getters, rootState, rootGetters) {
+      return state.items.map(item => ({
+        ...item,
+        price: rootGetters['stock/getPriceByName'](item.name)
+      }));
+    }
+  };
+  
